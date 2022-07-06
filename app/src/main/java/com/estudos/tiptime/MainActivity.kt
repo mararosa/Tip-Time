0511de2b181d9072edc8f6a1b7e65577cf43d20b
Mara <edimara.rosa@hotmail.com>
2022-07-05 22:08:51 -0300

    add listener to the keyboard

--- app/src/main/java/com/estudos/tiptime/MainActivity.kt
+++ app/src/main/java/com/estudos/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.estudos.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.estudos.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,9 +18,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateButton.setOnClickListener { calculateTip() }
+        setClickListeners()
     }
 
+
+    private fun setClickListeners() {
+        binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+    }
 
     private fun calculateTip() {
         // Get the decimal value from the cost of service text field
@@ -48,6 +57,17 @@ class MainActivity : AppCompatActivity() {
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
